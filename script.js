6821f0c03cef94f976bd46fb1dd82340126399ad
Nick Chapa <nicky.chapa@hotmail.com>
2026-05-11 11:59:48 -0500

    setup game, board, player objects

--- script.js
+++ script.js
@@ -1,1 +1,22 @@
-console.log("test");
+const gameboard = (() => {
+    const gameboardArray = [1, 2, 3];
+    const randVar = 42;
+
+    return {gameboardArray};
+})()
+
+function createPlayer(name, symbol) {
+
+    return {name, symbol}
+}
+
+const gameController = (() => {
+
+    const p1 = createPlayer("player1", "x");
+    const p2 = createPlayer("player2", "o");
+
+    const playersArray = [p1, p2];
+
+    console.log(gameboard.gameboardArray);
+    console.log(playersArray);
+})()
