decbf8e9f9ecab2cd686ef9ee5ca1375b5a5fed0
Nick Chapa <nicky.chapa@hotmail.com>
2026-05-14 12:23:16 -0500

    add initial user input logic

--- script.js
+++ script.js
@@ -1,8 +1,12 @@
 const gameboard = (() => {
-    const gameboardArray = [1, 2, 3];
+    const array = [
+        ['1A', '1B', '1C'],
+        ['2A', '2B', '2C'],
+        ['3A', '3B', '3C']
+    ];
     const randVar = 42;
 
-    return {gameboardArray};
+    return {array};
 })()
 
 const gameController = (() => {
@@ -24,4 +28,24 @@ const gameController = (() => {
 
     pickRandomPlayer(p1, p2);
     console.log(currentPlayer);
+    //console.log(gameboard[0][1]);
+
+    // round logic:
+    // take player input
+    // check if valid
+    // store in gameboard array
+    // reflect in display
+    // check for endgame conditions: tie or winner
+    function round(player){
+        const userInput = prompt("type index to put symbol");
+        if(userInput == '1A') gameboard.array[0][0] = player.symbol;
+        console.log(player.symbol);
+        if(player.name == 'player1') currentPlayer = p2;
+        else currentPlayer = p1;
+    }
+
+    round(currentPlayer);
+    console.log(gameboard.array);
+    round(currentPlayer);
+    console.log(gameboard.array);
 })()
